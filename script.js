67deb728edcb7580a744836af03c86197d7fc48e
Prem935 <prem886044@gmail.com>
2026-04-20 00:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let input1 = document.querySelector("#fname")
+	let input2 = document.querySelector("#lname")
 
+	alert(input1.value +" "+ input2.value)
 }
